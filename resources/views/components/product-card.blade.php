f1c92502f025befdec49b43a321a9e1c5360b212
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2023-04-18 08:41:06 -0700

    agregando carrusel a las imagenes de los productos  v83

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,5 +1,22 @@
 <div class="card">
-    <img class="card-img-top" src="{{ asset($product->images->first()->path) }}" alt="{{ $product->price }}" height="500px">
+
+    <div class="carousel slide carousel-fade" id="carousel{{$product->id}}">
+        <div class="carousel-inner">
+            @foreach ($product->images as $image)
+                <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                    <img class="d-block w-100 card-img-top" src="{{ asset($image->path) }}" alt="{{ $product->title }}" height="500">
+                </div>
+            @endforeach
+        </div>
+    
+        <a href="#carousel{{ $product->id }}" class="carousel-control-prev" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a href="#carousel{{ $product->id }}" class="carousel-control-next" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+    </div>
+    
     <div class="card-body">
         <h4 class="text-right"><strong> ${{ $product->price }} </strong></h4>
         <h5 class="card-title"> {{ $product->title }} </h5>
