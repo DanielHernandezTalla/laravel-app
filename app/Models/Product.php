7de32d21424888737e8ee0f0e81d3972b97f9e76
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2023-04-19 08:09:04 -0700

    usando soft deleting para remover productos v98

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Scopes\AvailableScope;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     // Asignas un nombre a la tabla
     protected $table = 'products';
