d7456a9bed157d615e02898cbcf7ae328fca75b7
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2023-04-19 09:03:21 -0700

    creadno comando para eliminar carritos avandonados v102

---
+++ app/Console/Commands/RemoveOldCarts.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Cart;
+use Illuminate\Console\Command;
+
+class RemoveOldCarts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'carts:remove-old {--days=7 : The days after which the carts will be removed}';
+    // php artisan carts:remove-old --days=10
+    // php artisan carts:remove-old --help
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove old carts from a given set of days.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $deadline = now()->subDays($this->option('days'));
+
+        $counter = Cart::whereDate('updated_at', '<=', $deadline)->delete();
+
+        $this->info("Done! {$counter} carts were removed");
+    }
+}
